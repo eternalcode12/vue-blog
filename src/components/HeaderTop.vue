897efe0e36028ee2db0c4f48287915b8ee5ec66f
eternalcode12 <eternalcode@163.com>
2021-06-16 17:10:33 +0800

    完成基本的配置

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header-top">
+    <div class="ww">
+      <div class="logo">ETERNALCODER</div>
+      <ul class="navigation">
+        <li v-for="(navigator, index) of navigators" :key="index">
+          <template>
+            <div>
+              <a href="navigator.url">{{ navigator.name }}</a>
+              <i
+                v-if="navigator.flag"
+                class="fa fa-angle-down"
+                aria-hidden="true"
+              ></i>
+            </div>
+          </template>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "HeaderTop",
+  computed: {
+    ...mapState({
+      navigators: (state) => state.home.navigators,
+    }),
+  },
+  created() {
+    console.log(this.navigators);
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+@import "@/style/header-top.scss";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <header-top></header-top>
+    <banner></banner>
+  </div>
+</template>
+
+<script>
+import HeaderTop from "@/components/HeaderTop";
+import Banner from "@/components/home/Banner";
+export default {
+  name: "Home",
+  components: {
+    HeaderTop,
+    Banner,
+  },
+};
+</script>
