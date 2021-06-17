944a676f9461b78a9df4fc15e5d444c23755b2ec
eternalcode12 <eternalcode@163.com>
2021-06-17 14:33:26 +0800

    css 完成hover显示遮罩层

--- src/components/home/PhotoWall.vue
+++ src/components/home/PhotoWall.vue
@@ -3,33 +3,38 @@
     <div class="ww">
       <div class="left">
         <template v-for="(leftPic, index) of pics[0]">
-          <div :key="index">
+          <div class="template" :key="index">
             <img :src="leftPic.url" :alt="leftPic.alt" class="imgList" />
-            <div class="title"></div>
+            <div class="title">{{ leftPic.title }}</div>
+            <div class="content">{{ leftPic.content }}</div>
           </div>
         </template>
       </div>
       <div class="middle">
         <template v-for="(middlePic, index) of pics[1]">
-          <div :key="index">
+          <div class="template" :key="index">
             <img
               :key="index"
               :src="middlePic.url"
               :alt="middlePic.alt"
               class="imgList"
             />
+            <div class="title">{{ middlePic.title }}</div>
+            <div class="content">{{ middlePic.content }}</div>
           </div>
         </template>
       </div>
       <div class="right">
         <template v-for="(rightPic, index) of pics[2]">
-          <div :key="index">
+          <div class="template" :key="index">
             <img
               :key="index"
               :src="rightPic.url"
               :alt="rightPic.alt"
               class="imgList"
             />
+            <div class="title">{{ rightPic.title }}</div>
+            <div class="content">{{ rightPic.content }}</div>
           </div>
         </template>
       </div>
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -52,33 +52,49 @@ const state = {
   pics: [
     [{
         url: require('@/assets/images/img-1.png'),
-        alt: 'img-1.png'
+        alt: 'img-1.png',
+        title: 'Branding',
+        content: 'NONSENSICAL CONTENT'
       }, {
         url: require('@/assets/images/img-2.png'),
-        alt: 'img-2.png'
+        alt: 'img-2.png',
+        title: 'Card',
+        content: 'THEREFORE ALWAYS'
       },
       {
         url: require('@/assets/images/img-4.png'),
-        alt: 'img-4.png'
+        alt: 'img-4.png',
+        title: 'Card',
+        content: 'SOMETIME ACTIVE'
       }
     ],
     [{
       url: require('@/assets/images/img-8.png'),
-      alt: 'img-8.png'
+      alt: 'img-8.png',
+      title: 'Coffee',
+      content: 'PAGEMAKER INCLUDING'
     }, {
       url: require('@/assets/images/img-7.png'),
-      alt: 'img-7.png'
-    }, ],
+      alt: 'img-7.png',
+      title: 'Bottle',
+      content: 'THEREFORE ALWAYS'
+    }],
     [{
         url: require('@/assets/images/img-5.png'),
-        alt: 'img-5.png'
+        alt: 'img-5.png',
+        title: 'Lebles',
+        content: 'SOMETIME ACTIVE'
       }, {
         url: require('@/assets/images/img-6.png'),
-        alt: 'img-6.png'
+        alt: 'img-6.png',
+        title: 'Pepers',
+        content: 'THEREFORE ALWAYS'
       },
       {
         url: require('@/assets/images/img-3.png'),
-        alt: 'img-3.png'
+        alt: 'img-3.png',
+        title: 'Milk',
+        content: 'SOMETIME ACTIVE'
       }
     ],
   ],
